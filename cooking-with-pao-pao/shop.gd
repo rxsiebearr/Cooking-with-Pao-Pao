f30c5f3b9cdcfc085dbab90bf5da3940bcb6acc7
keliang5 <159198151+keliang5@users.noreply.github.com>
2025-03-26 10:36:06 -0700

    Menu Done

--- cooking-with-pao-pao/shop.gd
+++ cooking-with-pao-pao/shop.gd
@@ -1,20 +1,18 @@
 extends Area2D
 
-var is_Player_near := false
+@onready var shop_menu: CanvasLayer = $Shop_Menu
+var is_player_near := false
 
 func _ready() -> void:
 	body_entered.connect(func (body: Node) -> void:
-		is_Player_near = true
+		is_player_near = true
 	)
 	body_exited.connect(func (body: Node) -> void:
-		is_Player_near = false
+		is_player_near = false
 	)
 
-
-#func _unhandled_input(event: InputEvent) -> void:
-#	if is_Player_near and event.is_action_pressed("interact"):
-#		get_node("Shop_Menu/AnimationPlayer").play("TransIn")
-
-
-func _on_body_entered(body: Node2D) -> void:
-	get_node("Shop_Menu/AnimationPlayer").play("TransIn")
+func _input(event) -> void:
+	if is_player_near and event.is_action_pressed("interact"):
+		get_tree().paused = true
+		shop_menu.visible = true
+		
--- cooking-with-pao-pao/test.gd
+++ cooking-with-pao-pao/test.gd
@@ -2,7 +2,7 @@ class_name Player
 extends CharacterBody2D
 
 
-@export var max_speed := 300.0
+@export var max_speed := 600.0
 
 func _physics_process(_delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
