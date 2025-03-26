4a6b54d8b03b71d87420ec400f00dd3dc6129e06
rxsiebearr <chzou1@s.sfusd.edu>
2025-03-26 11:09:52 -0700

    interaction area on players

--- cooking-with-pao-pao/bear_bear.gd
+++ cooking-with-pao-pao/bear_bear.gd
@@ -4,9 +4,11 @@ extends CharacterBody2D
 @export var drag_factor := 10.0
 @export var max_health := 15
 @onready var collision_shape_2d: CollisionShape2D = %CollisionShape2D
+
 @onready var item_drop = load("res://item_drop.tscn")
 @onready var character_sprite: Sprite2D = $CharacterSprite
 @onready var item_sprite: Sprite2D = $ItemSprite
+
 
 var carrying_item: bool = false
 var drop_pos: Vector2
@@ -45,13 +47,13 @@ func drop_item():
 	get_parent().add_child(item)
 	carrying_item = false
 
-func _on_pickup_area_area_entered(area: Area2D) -> void:
-	if area.is_in_group("item_drop"):
-		items_in_range.append(area)
+#func _on_pickup_area_area_entered(area: Area2D) -> void:
+#	if area.is_in_group("item_drop"):
+#		items_in_range.append(area)
 
-func _on_pickup_area_area_exited(area: Area2D) -> void:
-	if area.is_in_group("item_drop"):
-		items_in_range.erase(area)
+#func _on_pickup_area_area_exited(area: Area2D) -> void:
+#	if area.is_in_group("item_drop"):
+#		items_in_range.erase(area)
 		
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
