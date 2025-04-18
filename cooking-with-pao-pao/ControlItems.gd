a4fd12c0cc505a30cff51b6d3a1d50d04e5ab6ae
Purple9200 <anxie8@s.sfusd.edu>
2025-04-18 11:15:53 -0700

    Added Buttons (No Code)

--- cooking-with-pao-pao/ControlItems.gd
+++ cooking-with-pao-pao/ControlItems.gd
@@ -2,6 +2,14 @@ extends Control
 
 @onready var item_scene = preload("res://ShopItem.tscn")
 @onready var items_grid = $"Panel/Shop Items"
+@onready var sell: Button = $VBoxContainer/Sell
+@onready var sell_2: Button = $VBoxContainer/Sell2
+@onready var sell_3: Button = $VBoxContainer/Sell3
+@onready var sell_4: Button = $VBoxContainer/Sell4
+@onready var buy: Button = $VBoxContainer/Buy
+@onready var buy_2: Button = $VBoxContainer/Buy2
+@onready var buy_3: Button = $VBoxContainer/Buy3
+@onready var buy_4: Button = $VBoxContainer/Buy4
 
 var item_paths = [
 	"res://Rice.tres",
