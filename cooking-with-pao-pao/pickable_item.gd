e55e5941bacf79456578a9ee2ca85bc63bf28808
rxsiebearr <chzou1@s.sfusd.edu>
2025-04-25 11:02:09 -0700

    resize images

--- cooking-with-pao-pao/pickable_item.gd
+++ cooking-with-pao-pao/pickable_item.gd
@@ -4,5 +4,6 @@ class_name pickable_item
 @export var item_texture: Texture
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @export var item_name: String = "Unknown"
+
 func _ready() -> void:
 	sprite_2d.texture = item_texture
