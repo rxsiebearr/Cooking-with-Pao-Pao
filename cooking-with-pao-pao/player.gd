e19beaaf62709ecedbe39b4c216220813fd03e91
rxsiebearr <chzou1@s.sfusd.edu>
2025-05-14 11:40:09 -0700

    minimap and update characters

--- cooking-with-pao-pao/player.gd
+++ cooking-with-pao-pao/player.gd
@@ -37,9 +37,9 @@ func _physics_process(delta: float) -> void:
 	var steering := desired_velocity - velocity
 	velocity += steering * drag_factor * delta
 	move_and_slide()
-	GlobalData.player_position = position
+	GlobalData.player_position = global_position
 	print("player:")
-	print(position)
+	print(global_position)
 	
 	if direction.x > 0:
 		character_sprite.flip_h = false
--- cooking-with-pao-pao/sub_viewport.gd
+++ cooking-with-pao-pao/sub_viewport.gd
@@ -1,9 +1,14 @@
 extends SubViewport
 
 @onready var camera_2d = $Camera2D
+@onready var sprite_2d: Sprite2D = $"../Sprite2D"
 
 func _physics_process(_delta):
 	if GlobalData.player_instantiated:
-		camera_2d.position = GlobalData.player_position
+		camera_2d.global_position = GlobalData.player_position
 		print("camera:")
-		print(camera_2d.position)
+		print(camera_2d.global_position)
+	if camera_2d.global_position.y >= 2188.074:
+		sprite_2d.position.y = 144.0
+	else: 
+		sprite_2d.position.y = 72.0
