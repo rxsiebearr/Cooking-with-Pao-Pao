3c318359401cab551c04f49fa2ead9b0e6435a93
rxsiebearr <chzou1@s.sfusd.edu>
2025-05-14 10:39:28 -0700

    fixed character selection theme

--- cooking-with-pao-pao/character_slot.gd
+++ cooking-with-pao-pao/character_slot.gd
@@ -11,8 +11,16 @@ func set_selected(boolean):
 	
 	if boolean:
 		stylebox.border_color = Color(0.2235, 0.4314, 0.9529)
+		stylebox.expand_margin_left = 4.0
+		stylebox.expand_margin_top  = 4.0
+		stylebox.expand_margin_right = 4.0
+		stylebox.expand_margin_bottom = 4.0
 	else:
 		stylebox.border_color = Color(0.32, 0.55, 0.78)
+		stylebox.expand_margin_left = 0.0
+		stylebox.expand_margin_top  = 0.0
+		stylebox.expand_margin_right = 0.0
+		stylebox.expand_margin_bottom = 0.0
 		
 	add_theme_stylebox_override("panel", stylebox)
 	
