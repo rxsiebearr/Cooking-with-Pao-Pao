c8416106bca371dbcab9620e4af11a95387d2d82
keliang5 <159198151+keliang5@users.noreply.github.com>
2025-05-15 11:30:32 -0700

    Getting Order Readdded

--- cooking-with-pao-pao/store_#1.gd
+++ cooking-with-pao-pao/store_#1.gd
@@ -4,9 +4,22 @@ extends Node2D
 @onready var readycook: CanvasLayer = $CanvasLayer2
 @onready var day_timer: Timer = $DayTimer
 
+var is_timer_active = false
 
 func _ready() -> void:
 	store.visible = false
+
+
+func _process(delta: float) -> void:
+	if not is_timer_active:
+		get_orders()
+
+func get_orders():
+	is_timer_active = true 
+
+	await get_tree().create_timer(randi_range(5, 11)).timeout
+	Global.orders += 1
+	is_timer_active = false
 
 func _on_yes_button_pressed() -> void:
 	readycook.visible = false
