f68c767f6ab570c7f97d31cc3971525167a80fb0
Purple9200 <159182392+Purple9200@users.noreply.github.com>
2025-05-16 11:11:56 -0700

    Fridge Done

--- cooking-with-pao-pao/Global.gd
+++ cooking-with-pao-pao/Global.gd
@@ -5,7 +5,7 @@ var orders = 0
 var money_made = 0
 var orders_served = 0
 var rice_seeds = 0
-var rice = 0
+var rice = 1
 
 func _process(_delta: float) -> void:
 	if orders < 0:
---
+++ cooking-with-pao-pao/fridge_area.gd
@@ -0,0 +1,6 @@
+extends Area2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+func fridge_open():
+	animation_player.play("Fridge_Open")
+func fridge_closed():
+	animation_player.play("Fridge_Close")
