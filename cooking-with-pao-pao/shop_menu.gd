0930493468612e55fd7827097a5270fb05f4615b
Purple9200 <159182392+Purple9200@users.noreply.github.com>
2025-05-16 13:30:43 -0700

    Added background music to game and store

--- cooking-with-pao-pao/shop_menu.gd
+++ cooking-with-pao-pao/shop_menu.gd
@@ -1,7 +1,9 @@
 extends CanvasLayer
 @onready var panel: Panel = $Panel
+@onready var Shop_menu_music: AudioStreamPlayer2D = $AudioStreamPlayer2D
 @onready var shop: CanvasLayer = $"."
-
+func _ready():
+	Shop_menu_music.play()
 func _process(delta: float) -> void:
 	get_node("Label").text = "x" + str(Global.money)
 
